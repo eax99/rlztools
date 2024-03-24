495b3363f9aaa09ca5bf55164d8fa4ef7e3b29c3
eax99 <152572773+eax99@users.noreply.github.com>
2024-03-24 22:29:41 +0200

    Add license headers to test scripts

--- test/test-rlzparse.sh
+++ test/test-rlzparse.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+# SPDX-License-Identifier: MPL-2.0
+# Copyright 2024 Eve Kivivuori
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 # Params: width, SA width, input, dictionary, SA, format, expected output.
 # Wrapper around compress_compare to pretty-print the inputs and result.
